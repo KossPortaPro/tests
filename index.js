0a99fd0c78430002ceb4bcbf302e72d87ff34090
PaulMuller <paul@dao-group.org>
2024-05-14 18:57:16 +0300

    test

--- index.js
+++ index.js
@@ -1,40 +1,12 @@
-const noble = require('noble')
+const { exec } = require('child_process');
 
-noble.on('stateChange', function(state) {
-  if (state === 'poweredOn') {
-    noble.startScanning();
-  } else {
-    noble.stopScanning();
+// Execute bluetoothctl command to scan for nearby devices
+exec('bluetoothctl scan on', (error, stdout, stderr) => {
+  if (error) {
+    console.error(`Error executing command: ${error}`);
+    return;
   }
+  
+  console.log(`Scan output: ${stdout}`);
+  console.error(`Scan errors: ${stderr}`);
 });
-
-
-
-noble.on('discover', function(peripheral) {
-    console.log('peripheral discovered (' + peripheral.id +
-                ' with address <' + peripheral.address +  ', ' + peripheral.addressType + '>,' +
-                ' connectable ' + peripheral.connectable + ',' +
-                ' RSSI ' + peripheral.rssi + ':');
-    console.log('\thello my local name is:');
-    console.log('\t\t' + peripheral.advertisement.localName);
-    console.log('\tcan I interest you in any of the following advertised services:');
-    console.log('\t\t' + JSON.stringify(peripheral.advertisement.serviceUuids));
-  
-    var serviceData = peripheral.advertisement.serviceData;
-    if (serviceData && serviceData.length) {
-      console.log('\there is my service data:');
-      for (var i in serviceData) {
-        console.log('\t\t' + JSON.stringify(serviceData[i].uuid) + ': ' + JSON.stringify(serviceData[i].data.toString('hex')));
-      }
-    }
-    if (peripheral.advertisement.manufacturerData) {
-      console.log('\there is my manufacturer data:');
-      console.log('\t\t' + JSON.stringify(peripheral.advertisement.manufacturerData.toString('hex')));
-    }
-    if (peripheral.advertisement.txPowerLevel !== undefined) {
-      console.log('\tmy TX power level is:');
-      console.log('\t\t' + peripheral.advertisement.txPowerLevel);
-    }
-  
-    console.log();
-  });
